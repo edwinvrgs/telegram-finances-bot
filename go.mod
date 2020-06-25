04531dd98bc2d99f8a20a83fecc948a1292b7f99
Edwin Vargas <edwinleandrovargas@gmail.com>
2020-06-24 21:00:06 -0400

    Try new approach

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/edwinvrgs/telegram-finances-bot
+
+go 1.14
+
+require (
+	github.com/pkg/errors v0.9.1 // indirect
+	gopkg.in/tucnak/telebot.v2 v2.3.2
+)
--- main.go
+++ main.go
@@ -1,89 +1,35 @@
-package bot
+package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"errors"
-	"fmt"
-	"net/http"
+	"log"
 	"os"
-	"strings"
+
+	tb "gopkg.in/tucnak/telebot.v2"
 )
 
-// Create a struct that mimics the webhook response body
-// https://core.telegram.org/bots/api#update
-type webhookReqBody struct {
-	Message struct {
-		Text string `json:"text"`
-		Chat struct {
-			ID int64 `json:"id"`
-		} `json:"chat"`
-	} `json:"message"`
-}
+func main() {
+	var (
+		port      = os.Getenv("PORT")
+		publicURL = os.Getenv("PUBLIC_URL")
+		token     = os.Getenv("TOKEN")
+	)
 
-// This handler is called everytime telegram sends us a webhook event
-func Handler(res http.ResponseWriter, req *http.Request) {
-	// First, decode the JSON response body
-	body := &webhookReqBody{}
-	if err := json.NewDecoder(req.Body).Decode(body); err != nil {
-		fmt.Println("Could not decode request body", err)
-		return
+	webhook := &tb.Webhook{
+		Listen:   ":" + port,
+		Endpoint: &tb.WebhookEndpoint{PublicURL: publicURL},
 	}
 
-	// Check if the message contains the word "marco"
-	// if not, return without doing anything
-	if !strings.Contains(strings.ToLower(body.Message.Text), "marco") {
-		return
+	pref := tb.Settings{
+		Token:  token,
+		Poller: webhook,
 	}
 
-	// If the text contains marco, call the `sayPolo` function, which
-	// is defined below
-	if err := sayPolo(body.Message.Chat.ID); err != nil {
-		fmt.Println("error in sending reply:", err)
-		return
-	}
-
-	// log a confirmation message if the message is sent successfully
-	fmt.Println("reply sent")
-}
-
-//The below code deals with the process of sending a response message
-// to the user
-
-// Create a struct to conform to the JSON body
-// of the send message request
-// https://core.telegram.org/bots/api#sendmessage
-type sendMessageReqBody struct {
-	ChatID int64  `json:"chat_id"`
-	Text   string `json:"text"`
-}
-
-// sayPolo takes a chatID and sends "polo" to them
-func sayPolo(chatID int64) error {
-	// Create the request body struct
-	reqBody := &sendMessageReqBody{
-		ChatID: chatID,
-		Text:   "Polo!!",
-	}
-	// Create the JSON body from the struct
-	reqBytes, err := json.Marshal(reqBody)
+	b, err := tb.NewBot(pref)
 	if err != nil {
-		return err
+		log.Fatal(err)
 	}
 
-	// Send a post request with your token
-	res, err := http.Post("https://api.telegram.org/bot"+os.Getenv("PORT")+"/sendMessage", "application/json", bytes.NewBuffer(reqBytes))
-	if err != nil {
-		return err
-	}
-	if res.StatusCode != http.StatusOK {
-		return errors.New("unexpected status" + res.Status)
-	}
-
-	return nil
-}
-
-// Finally, the webhook funtion starts our server on port 80
-func bot() {
-	http.ListenAndServe(os.Getenv("PORT"), http.HandlerFunc(Handler))
+	b.Handle("/hello", func(m *tb.Message) {
+		b.Send(m.Sender, "Hi!")
+	})
 }
